2e3ff91e5f6a040545094bc192b84ca648ccbe04
ridha <ridhanshari@gmail.com>
2022-12-21 14:43:37 +0700

    add requirement deploy model

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, jsonify
-
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'uploadedFiles'
+from prepro.textPrepro import TextProcessing
+from inference.predict import PredictSentiment
 
 from flask import request
 from flasgger import Swagger, LazyString, LazyJSONEncoder
@@ -9,72 +8,109 @@ from flasgger import swag_from
 from werkzeug.utils import secure_filename
 import os.path
 
+TP = TextProcessing()
+predict_model = PredictSentiment()
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploadedFiles'
+
+
 app.json_encoder = LazyJSONEncoder
 swagger_template = dict(
-    info = {
+    info={
         'title': LazyString(lambda: 'Text Labeling API with LSTM and ANN model'),
         'version': LazyString(lambda: '1.0.0'),
         'description': LazyString(lambda: 'This API for Text Labeling'),
     },
-    host = LazyString(lambda: request.host),
+    host=LazyString(lambda: request.host),
 )
 swagger_config = {
     'headers': [],
     'specs': [
         {
-        'endpoint': 'docs',
-        'route': '/docs.json',
+            'endpoint': 'docs',
+            'route': '/docs.json',
         }
     ],
     'static_url_path': '/flasgger_static',
     'swagger_ui': True,
     'specs_route': '/docs/',
 }
-swagger = Swagger(app, template=swagger_template, 
+swagger = Swagger(app, template=swagger_template,
                   config=swagger_config)
+
+
+def mapping_result(result_prediction):
+    if result_prediction == 0:
+        return "neutral"
+    elif result_prediction == 1:
+        return "positive"
+    else:
+        return "negative"
 
 
 @swag_from('docs/annDocsAPI.yml', methods=['POST'])
 @app.route('/ann-model', methods=['POST'])
 def ANNpost_file():
-    
+
     reqFile = request.files['upfile']
     fileName = secure_filename(reqFile.filename)
     reqFile.save(os.path.join(
-                app.config['UPLOAD_FOLDER'],
-                fileName
-            ))
-    with open(r"uploadedFiles/{}".format(reqFile.filename),"r+") as f:
+        app.config['UPLOAD_FOLDER'],
+        fileName
+    ))
+    with open(r"uploadedFiles/{}".format(reqFile.filename), "r+") as f:
         data = f.read()
-    
+        f.seek(0)
+        cleanText, bow = TP.get_bow(data)
+        result_prediction = predict_model.predict_ann(bow)
+        for i, x in enumerate(result_prediction):
+            if(i == 0 and x == 1):
+                hasil = "negative"
+            elif(i == 1 and x == 1):
+                hasil = "positive"
+            elif(i == 2 and x == 1):
+                hasil = "neutral"
+        f.write(cleanText)
+        f.write(hasil)
+        f.truncate()
+
     json_response = {
-        'result': data,
+        'text': cleanText,
+        'sentiment result': hasil,
     }
-    
+
     response_data = jsonify(json_response)
     return response_data
+
 
 @swag_from('docs/lstmDocsAPI.yml', methods=['POST'])
 @app.route('/lstm-model', methods=['POST'])
 def LSTMpost_file():
-    
+
     reqFile = request.files['upfile']
     fileName = secure_filename(reqFile.filename)
     reqFile.save(os.path.join(
-                app.config['UPLOAD_FOLDER'],
-                fileName
-            ))
-    with open(r"uploadedFiles/{}".format(reqFile.filename),"r+") as f:
+        app.config['UPLOAD_FOLDER'],
+        fileName
+    ))
+    with open(r"uploadedFiles/{}".format(reqFile.filename), "r+") as f:
         data = f.read()
-    
+        f.seek(0)
+        clean_text, input_ids = TP.get_tokenizer(data)
+        result_prediction = predict_model.predict_lstm(input_ids)
+        result_prediction = mapping_result(result_prediction)
+        f.write(clean_text)
+        f.write(result_prediction)
+        f.truncate()
+
     json_response = {
-        'result': data,
+        'text': clean_text,
+        'sentiment result': result_prediction,
     }
-    
+
     response_data = jsonify(json_response)
     return response_data
 
 
-
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ prepro/textPrepro.py
@@ -0,0 +1,60 @@
+import re
+import pickle
+import pandas as pd
+import csv
+import os
+from nltk.corpus import stopwords
+
+from keras_preprocessing.sequence import pad_sequences
+
+
+class TextProcessing():
+    def __init__(self) -> None:
+        with open(os.path.join("models", "count_vect.pkl"), 'rb') as f:
+            self.count_vect = pickle.load(f)
+
+        with open(os.path.join("models", "tf_tranformer.pkl"), 'rb') as f:
+            self.tf_transformer = pickle.load(f)
+
+        with open(os.path.join("models", 'tokenizer.pkl'), 'rb') as f:
+            self.tokenizer = pickle.load(f)
+
+        self.dfAlay = csv.reader(open(os.path.join("dataset", "new_kamusalay.csv"), 'r'))
+        self.dfAlay = pd.DataFrame(self.dfAlay, columns=['original', 'replacement'])
+        
+        pass
+
+    # remove kata alay
+    def normalize_alay(self, line) -> str:
+        alay_dict_map = dict(
+            zip(self.dfAlay['original'], self.dfAlay['replacement']))
+        return ' '.join([alay_dict_map[word] if word in alay_dict_map else word for word in line.split(' ')])
+
+    # remove punctuation
+    def remove_punctuation(self, line) -> str:
+        return re.sub(r'[^\w\s]', ' ', line)
+
+    # remove stopword
+    def remove_stopword(self, line) -> str:
+        stopword_list = set(stopwords.words('indonesian'))
+        return ' '.join([word for word in line.split(' ') if word not in stopword_list])
+
+    def get_bow(self, text):
+        text = text.lower()
+        text = self.normalize_alay(text)
+        text = self.remove_punctuation(text)
+        text = self.remove_stopword(text)
+        clean_text = text
+        new_df = pd.DataFrame([text], columns=['text'])
+        target_predict = self.count_vect.transform(new_df['text'])
+        target_predict = self.tf_transformer.transform(target_predict)
+        return clean_text, target_predict
+
+    def get_tokenizer(self, text) -> list:
+        text = text.lower()
+        text = self.normalize_alay(text)
+        text = self.remove_punctuation(text)
+        text = self.remove_stopword(text)
+        clean_text = text
+        text = self.tokenizer.texts_to_sequences([text])
+        return clean_text, pad_sequences(text, maxlen=200)
