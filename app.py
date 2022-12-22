a5563bb314a27276c2b2a7a35252a22c18b0b212
ridha <ridhanshari@gmail.com>
2022-12-22 11:32:08 +0700

    create database file to save result

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 from flask import Flask, jsonify
 from prepro.textPrepro import TextProcessing
 from inference.predict import PredictSentiment
@@ -39,6 +41,11 @@ swagger = Swagger(app, template=swagger_template,
                   config=swagger_config)
 
 
+def get_db_connection():
+    conn = sqlite3.connect(os.path.join("database", "platChallange.db"))
+    conn.row_factory = sqlite3.Row
+    return conn
+
 def mapping_result(result_prediction):
     if result_prediction == 0:
         return "neutral"
@@ -51,7 +58,9 @@ def mapping_result(result_prediction):
 @swag_from('docs/annDocsAPI.yml', methods=['POST'])
 @app.route('/ann-model', methods=['POST'])
 def ANNpost_file():
-
+    
+    conn = get_db_connection()
+    
     reqFile = request.files['upfile']
     fileName = secure_filename(reqFile.filename)
     reqFile.save(os.path.join(
@@ -66,15 +75,20 @@ def ANNpost_file():
         result_prediction = predict_model.predict_ann(bow)
         for i, x in enumerate(result_prediction):
             if(i == 0 and x == 1):
-                hasil = "negative"
+                hasil = "neutral"
             elif(i == 1 and x == 1):
                 hasil = "positive"
             elif(i == 2 and x == 1):
-                hasil = "neutral"
+                hasil = "negative"
         f.write(cleanText)
+        f.write('|')
         f.write(hasil)
         f.truncate()
 
+    conn.execute('INSERT INTO annModel (oriText, cleanText, label) VALUES (?, ?, ?)', (data, cleanText, hasil))
+    conn.commit()
+    conn.close()
+    
     json_response = {
         'text': cleanText,
         'sentiment result': hasil,
@@ -87,16 +101,23 @@ def ANNpost_file():
 @app.route('/ann-model-text', methods=['POST'])
 def ANNtextpredict():
     
+    conn = get_db_connection()
+    
     text = request.form.get('text')
     clean_text,bow = TP.get_bow(text)
     result_prediction = predict_model.predict_ann(bow)
     for i, x in enumerate(result_prediction):
         if(i == 0 and x == 1):
-            hasil = "negative"
+            hasil = "neutral"
         elif(i == 1 and x == 1):
             hasil = "positive"
         elif(i == 2 and x == 1):
-            hasil = "neutral"
+            hasil = "negative"
+    
+    conn.execute('INSERT INTO annModel (oriText, cleanText, label) VALUES (?, ?, ?)', (text, clean_text, hasil))
+    conn.commit()
+    conn.close()
+    
     return jsonify({"text":clean_text, "result_sentiment":hasil})
 
 
@@ -104,6 +125,8 @@ def ANNtextpredict():
 @app.route('/lstm-model', methods=['POST'])
 def LSTMpost_file():
 
+    conn = get_db_connection()
+    
     reqFile = request.files['upfile']
     fileName = secure_filename(reqFile.filename)
     reqFile.save(os.path.join(
@@ -118,9 +141,14 @@ def LSTMpost_file():
         result_prediction = predict_model.predict_lstm(input_ids)
         result_prediction = mapping_result(result_prediction)
         f.write(clean_text)
+        f.write('|')
         f.write(result_prediction)
         f.truncate()
 
+    conn.execute('INSERT INTO lstmModel (oriText, cleanText, label) VALUES (?, ?, ?)', (data, clean_text, result_prediction))
+    conn.commit()
+    conn.close()
+    
     json_response = {
         'text': clean_text,
         'sentiment result': result_prediction,
@@ -133,11 +161,18 @@ def LSTMpost_file():
 @app.route('/lstm-model-text', methods=['POST'])
 def LSTMtextpredict():
     
+    conn = get_db_connection()
+    
     text = request.form.get('text')
     clean_text,input_ids = TP.get_tokenizer(text)
     result_prediction = predict_model.predict_lstm(input_ids)
     result_prediction = mapping_result(result_prediction)
+    
+    conn.execute('INSERT INTO lstmModel (oriText, cleanText, label) VALUES (?, ?, ?)', (text, clean_text, result_prediction))
+    conn.commit()
+    conn.close()
+    
     return jsonify({"text":clean_text, "result_sentiment":result_prediction})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=1234)
